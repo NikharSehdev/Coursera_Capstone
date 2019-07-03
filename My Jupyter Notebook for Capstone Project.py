f19395eff036f2b2b8bb362f755881e316b59811
NikharSehdev <52480554+NikharSehdev@users.noreply.github.com>
2019-07-03 20:33:22 +0530

    Add files via upload

---
+++ My Jupyter Notebook for Capstone Project.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# <font color = 'blue'>I am Nikhar and This is my first notebook for the IBM APPLIED DATA SCIENCE CAPSTONE PROJECT and I am excited for the milestones ahead in this project</font>
+
+# In[5]:
+
+
+import numpy as np
+import pandas as pd
+a = "Hello Capstone Project Course!"
+print(a)
+
